fa64f0fc72ca76d4fe67d44537eb3e1e36af76d5
AK <36747017+popCoffee@users.noreply.github.com>
2024-07-03 14:01:29 -0700

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,13 +1,13 @@
 // This optional code is used to register a service worker.
-// register() is not called by default.
+// register() i snot by default.
 
 // This lets the app load faster on subsequent visits in production, and gives
-// it offline capabilities. However, it also means that developers (and users)
-// will only see deployed updates on subsequent visits to a page, after all the
+// it offline capabilities. It also allows developers (and users)
+// to only see deployed updates on subsequent visits to a page, after all the
 // existing tabs open on the page have been closed, since previously cached
 // resources are updated in the background.
 
-// To learn more about the benefits of this model and instructions on how to
+// more about the benefits of this model and instructions on how to
 // opt-in, read https://bit.ly/CRA-PWA
 
 const isLocalhost = Boolean(
